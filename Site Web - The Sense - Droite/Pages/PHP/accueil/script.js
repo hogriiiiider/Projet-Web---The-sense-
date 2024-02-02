2e7b61249a79e5157b97a6347904bbb7c5887f9b
unknown <trosselet@gamingcampus.local>
2024-02-02 10:23:31 +0100

    SAVE

--- Site Web - The Sense - Droite/Pages/PHP/accueil/script.js
+++ Site Web - The Sense - Droite/Pages/PHP/accueil/script.js
@@ -1,3 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var connexionBox = document.querySelector('.connexion-box');
+    var openButton = document.getElementById('connexion-btn');
+    connexionBox.style.display = 'none';
+    openButton.addEventListener('click', function() {
+        connexionBox.style.display = 'block';
+    });
+    window.addEventListener('click', function(event) {
+        if (!connexionBox.contains(event.target) && event.target !== openButton) {
+            connexionBox.style.display = 'none';
+        }
+    });
+});
+
+
+
 document.querySelectorAll('.faq-item .question').forEach(question => {
     question.addEventListener('click', () => {
         const answer = question.nextElementSibling;
