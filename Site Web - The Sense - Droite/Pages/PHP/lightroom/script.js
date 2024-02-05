b61738338d2ff48c63e09afac0f0a9a5bbe2e175
unknown <trosselet@gamingcampus.local>
2024-02-05 15:10:19 +0100

    LIGHTROOM

--- Site Web - The Sense - Droite/Pages/PHP/lightroom/script.js
+++ Site Web - The Sense - Droite/Pages/PHP/lightroom/script.js
@@ -42,3 +42,31 @@ $("#suivant").on("click",function(){
     document.getElementById("txt_carroussel").innerHTML = carroussel_txt[img];
     document.getElementById(rect_num[img]).src = rect_color[1];
 })
+
+
+//CONNEXION
+document.addEventListener('DOMContentLoaded', function() {
+    var connexionBox = document.querySelector('.connexion-box');
+    var openButton = document.getElementById('CONNECT');
+    connexionBox.style.display = 'none';
+    openButton.addEventListener('click', function() {
+        connexionBox.style.display = 'block';
+    });
+    window.addEventListener('click', function(event) {
+        if (!connexionBox.contains(event.target) && event.target !== openButton) {
+            connexionBox.style.display = 'none';
+        }
+    });
+});
+
+//VIDEO
+
+function openVideo() {
+    document.getElementById("overlayVideo").style.display = "flex";
+}
+
+function closeVideo() {
+    document.getElementById("overlayVideo").style.display = "none";
+}
+
+//VIDEO
