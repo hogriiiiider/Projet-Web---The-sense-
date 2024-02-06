1fa1a93b61e3752bbe5c72836ac6d720467d1fde
unknown <fhart@gamingcampus.local>
2024-02-06 09:49:45 +0100

    propos

---
+++ Pages/PHP/propos/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var connexionBox = document.querySelector('.connexion-box');
+    var openButton = document.getElementById('CONNECT');
+    connexionBox.style.display = 'none';
+    openButton.addEventListener('click', function() {
+        connexionBox.style.display = 'block';
+    });
+    window.addEventListener('click', function(event) {
+        if (!connexionBox.contains(event.target) && event.target !== openButton) {
+            connexionBox.style.display = 'none';
+        }
+    });
+});
